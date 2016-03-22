e44ac821d4b92c6c60db9e398d2e9a51d737fdb4
Iravinota <wswangshuo@gmail.com>
2016-03-22 10:54:51 -0400

    把x的低n位全置1

--- chp2/2_68.c
+++ chp2/2_68.c
@@ -1,22 +1,29 @@
 /*************************************************************************
     > File Name: 2_68.c
     > Author: Eric
-    > Desc: 最高位右侧全为1 
+    > Desc: 把x的低n位全置1
     > Created Time: Tue 22 Mar 2016 10:41:16 AM EDT
  ************************************************************************/
 
 #include<stdio.h>
 
-int lower_bits(int x, int n) 
+/* 低n位全为1的值 */
+int lower_n_bits(int n) 
 {
 	return (((1 << (n - 1)) - 1) << 1) | 1;
+}
+
+/* 把x的低n位全置1 */
+int lower_bits(int x, int n)
+{
+	return x | lower_n_bits(n);
 }
 
 int main()
 {
 	int i = 0;
 	for (i = 1; i < 33; i++) {
-		printf("%x\n", lower_bits(0, i));
+		printf("%x\n", lower_n_bits(0, i));
 	}
 	return 0;
 }
