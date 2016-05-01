2df1917a332a7a636c4b08e3af5da585a2b16c0e
Iravinota <wswangshuo@gmail.com>
2016-05-01 02:26:27 -0400

    float_half

---
+++ chp2/2_93.c
@@ -0,0 +1,27 @@
+/*************************************************************************
+    > File Name: 2_93.c
+    > Author: Eric
+    > Desc: f/0.5 
+    > Created Time: Sun 01 May 2016 02:23:20 AM EDT
+ ************************************************************************/
+
+#include<stdio.h>
+
+typedef unsigned float_bits;
+
+float_bits float_half(float_bits f)
+{
+	unsigned sign = f >> 31;
+	unsigned exp = f >> 23 & 0xFF;
+	unsigned frac = f & 0x7FFFFF;
+
+	if (exp == 0xFF && frac != 0) {
+		return f;
+	} else if (exp == 0) {
+		frac >> 1;
+	} else {
+		exp >> 1;
+	}
+
+	return (sign << 31) | (exp << 23) | frac;
+}
